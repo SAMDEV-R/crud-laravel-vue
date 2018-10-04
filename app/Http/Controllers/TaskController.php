aeaa15783753b1e06dbe063c2da670affe83974d
SAM <sandresmartinezr@gmail.com>
2018-10-03 19:31:05 -0500

    Create seeder and factory. Basic set up in TaskController.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Task;
 
 class TaskController extends Controller
 {
@@ -13,7 +14,8 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        $tasks = Task::get();
+        return $tasks;
     }
 
     /**
@@ -45,7 +47,8 @@ class TaskController extends Controller
      */
     public function edit($id)
     {
-        //
+        $task = Task::findOrFail($id);
+        return $task;
     }
 
     /**
@@ -57,7 +60,8 @@ class TaskController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $task = Task::findOrFail($id);
+        $task->delete();
     }
 
     /**
